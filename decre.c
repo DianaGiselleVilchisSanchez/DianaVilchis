0f91ff311df155bfae592c7fed45e1f3e977aabf
Diana Vichis <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-05-05 00:05:04 -0600

    Decrease

---
+++ decre.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    int c = 5;
+    printf ("%d\n",c);
+    printf ("%d\n",c++);
+    printf ("%d\n",c);
+    int d = 10;
+    printf ("%d\n",d);
+    printf ("%d\n",--d);
+    printf ("%d\n",d);
+}
