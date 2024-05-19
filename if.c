f35baa5f43edaa57a10fdf0dca232bbc5c5797b8
Diana Vichis <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-05-18 23:28:24 -0600

    use if to know if a number is even

---
+++ if.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+    int numero =55;
+    if (numero %2 == 0){
+        printf ("el numero es par\n1");
+    }
+    printf ("Fin del programa\n");
+    return 0;
+    
+}
